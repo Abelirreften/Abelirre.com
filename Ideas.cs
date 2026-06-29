6f593e41aaa995f6df267d82130df1e5e15cf056
Abelirreften <abelirreften@gmail.com>
2026-06-29 17:17:30 +0200

    Ideas para el porfolio

---
+++ Ideas.cs
@@ -0,0 +1,48 @@
+/*
+
+- PANTALLA DE CARGA:
+    - Comandos en distintos lenguajes de programación
+    - Barra de progreso de la carga de la página web
+
+- INICIO:
+    - Nombre "Abel Ahímsa Martín" y abajo "Abelirre"
+    - Game Developer y Creador de Contenido en un texto un poco más pequeño.
+    - Descripción actual.
+    - Botones de "Conóceme" y "Mi Portfolio".
+    - Un icono de "scroll" para indicar que se puede scrollear (obviamente)
+    - Un texto en letras pequeñas abajo a la derecha con la ubicación, idioma y que estoy abierto a trabajo (Madrid, Es · Open to work)
+
+- SOBRE MI:
+    - Descripciones (más cortas) de mis estudios y creación de contenido.
+    - Mis habilidades como apartado extra que esté resaltado.
+    - Apartado para una foto mía
+    - Educación: Unas tarjetas con el nombre, descripción, logo de la institución educativa, lugar y fechas.
+        - Grado en Diseño y Desarrollo de Videojuegos (2023 - Actual)
+        - Inglés C1 (2026 - En preparación)
+        - Bachillerato de Ciencias y Tecnología (2021 - 2023)
+        - Certificados Oficiales (En este caso tener una tarjeta de ejemplo con "En progreso").
+    - Experiencia Laboral en Videojuegos (de momento none)
+    - Otra Experiencia Laboral (Tarjetas con puesto, empresa, tipo de jornada, fechas de inicio y fin y lugar)
+
+- GAME DEV:
+    - Tarjetas con mis proyectos. En cada tarjeta: imagen del proyecto, nombre, descripción, tecnologías usadas, duración del desarrollo, y enlaces a itch.io o GitHub (un botón que ponga "Jugar Ahora ->" que lleve a dicho enlace).
+    - Cada proyecto debe poder tener varias etiquetas que lo clasifiquen (Ej: pixel art, 2D, 3D, Unity, Unreal Engine, Acción, Puzzle, Exploración, Aventura, Estrategia, etc).
+    - Debe haber una tarjeta "Featured" o "Proyecto principal) que sea más grande, visible y con más protagonismo.
+    - Debe haber una tarjeta "In Development" para indicar proyectos en los que estoy trabajando actualmente (En este caso el TFG).
+
+- YOUTUBE:
+    - Cuadrícula con los últimos vídeos
+    - Vídeos destacados (Destacados de YouTube)
+    - Estadísticas del canal
+
+- MIS REDES:
+    - Cuadrícula con mis redes sociales. En cada tarjeta: imagen de la red social, nombre, descripción y enlace.
+    - Youtube, Twitch, X (Twitter), Discord, Linkedin, GitHub, Itch.io
+
+- CONTACTO:
+    - Se queda como está añadiendo Linkedin
+
+- FOOTER:
+    - Abelirre © 2026 · Game Developer y Creador de Contenido · Madrid
+
+*/
